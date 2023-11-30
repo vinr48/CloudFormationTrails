36f74f07c5a247e980d0d2c22d4f8153c8be36c7
vinr48 <120105543+vinr48@users.noreply.github.com>
2023-12-01 09:43:07 +1100

    Update secrets.go

--- secrets.go
+++ secrets.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	// Dummy API Key for Testing
-	apiKey := "dummy_api_key_1234567890"
+	apiKey := "d47e95bd-aejk-9567-oqkp-00f321fc4d8f"
 
 	// Print the API Key
 	fmt.Println("API Key:", apiKey)
